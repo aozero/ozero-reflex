72cde676245aa1b396638cf9950eba9f9449c1a7
Alexander Ozero <a.ozero@live.com>
2015-09-26 22:21:54 -0600

    Added beginnings of One Player activity and made 1 Player button start it

--- app/src/main/java/com/example/ozero_reflex/OnePlayerActivity.java
+++ app/src/main/java/com/example/ozero_reflex/OnePlayerActivity.java
@@ -1,25 +1,24 @@
 package com.example.ozero_reflex;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class OnePlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_one_player);
     }
 
     /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_one_player, menu);
         return true;
     }
     */
@@ -37,11 +36,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void startOnePlayer(View view)
-    {
-        Intent onePlayerIntent = new Intent(this, OnePlayerActivity.class);
-        startActivity(onePlayerIntent);
     }
 }
